ded62a9b2ad712b19d16ff8dc76f6e61496cbb40
Matthew Wootten <mmwootten@outlook.com>
2018-06-29 09:58:52 -0400

    Add the ability to find the orientation of particular shapes

---
+++ find_orientation.py
@@ -0,0 +1,46 @@
+from sympy import *
+import numpy as np
+import scipy.optimize
+
+w, x, y, θ = symbols('w x y θ')
+
+def norm(v):
+    (x, y) = v
+    return sqrt(x**2 + y**2)
+
+def dot(a, b):
+    return a[0] * b[0] + a[1] * b[1]
+
+def pushAlong(x, y):
+    scalar = dot(x, y) / dot(y, y)
+    return (scalar * y[0], scalar * y[1])
+
+def vec_minus(a, b):
+    return (a[0] - b[0], a[1] - b[1])
+
+def distance_point_line(x, y, θ):
+    a = (x, y)
+    b = (cos(θ), sin(θ))
+    c = pushAlong(a, b)
+    return norm(vec_minus(a, c))
+
+def error_term_point(val_x, val_y, val_w):
+    return val_w * distance_point_line(x, y, θ).subs('x', val_x).subs('y', val_y)
+
+def find_orientation(xs, ys, ws):
+    xs = xs - xs.mean()
+    ys = ys - ys.mean()
+    error_terms = sum(map(error_term_point, xs, ys, ws))
+    error_terms_f = lambdify(θ, error_terms)
+    min_theta = scipy.optimize.minimize(error_terms_f, 0)
+    return min_theta.x % np.pi
+
+def weighted_centroid(xs, ys, ws):
+    x = sum(xs * ws) / sum(ws)
+    y = sum(ys * ws) / sum(ws)
+    return (x, y)
+
+def weighted_centroid_image(image):
+    ys, xs = (image != np.nan).nonzero()
+    ws = image[ys, xs]
+    return weighted_centroid(xs, ys, ws)
