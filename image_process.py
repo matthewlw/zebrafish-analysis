31be2d22b78a3b9fcbb65b608ac0667ab01e171b
Matthew Wootten <mmwootten@outlook.com>
2018-07-05 16:07:00 -0400

    Add some documentation and new methods to the image processing utils

--- image_process.py
+++ image_process.py
@@ -5,6 +5,18 @@ import skimage.morphology
 import numpy as np
 
 def brightest_portion(image, fraction):
+    """
+    Take (approximately) the brightest ``fraction`` proportion of the pixels in
+    an image.
+
+    This method works by taking an index into the sorted representation of the
+    image. As a result, if the image has a large number of pixels set to the
+    same value, then the
+
+    Parameters
+    ----------
+    image : ndarray image
+    """
     inv = skimage.util.invert(image)
     cutoff = np.sort(inv.ravel())[int((1-fraction) * image.size)]
     return inv >= cutoff
@@ -17,12 +29,27 @@ def crop_to_nonzero(image, border=2):
     return image[min(ys)-border:max(ys)+border, min(xs)-border:max(xs)+border]
 
 def take_biggest_blob(image):
-    new_image = np.zeros_like(image)
-    integered = skimage.img_as_int(image)
-    labeled = skimage.measure.label(integered)
-    blobs = skimage.measure.regionprops(labeled, intensity_image=integered, cache=True)
+    new_image = np.ones_like(image)
+    inverted = skimage.util.invert(image)
+    labeled = skimage.measure.label(inverted)
+    blobs = skimage.measure.regionprops(labeled, cache=True)
     blobs_by_area = sorted(blobs, key=lambda blob: blob.area, reverse=True)
     biggest_blob = blobs_by_area[0]
     for [r, c] in biggest_blob.coords:
-        new_image[r, c] = True
+        new_image[r, c] = False
+    return new_image
+
+def get_blobs(binary_image, connectivity=2):
+    integered = skimage.img_as_int(binary_image)
+    labeled = skimage.measure.label(integered, connectivity=connectivity)
+    props = skimage.measure.regionprops(label_image=labeled, intensity_image=integered, cache=True)
+    return props
+
+def filter_blobs(image, blob_predicate, connectivity=2):
+    new_image = np.zeros_like(image)
+    blobs = get_blobs(image, connectivity=connectivity)
+    selected_blobs = filter(blob_predicate, blobs)
+    for selected_blob in selected_blobs:
+        for [r, c] in selected_blob.coords:
+            new_image[r, c] = True
     return new_image
